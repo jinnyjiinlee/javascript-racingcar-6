eb4b5c4e07d6323b7f974e979aa0cc558c344983
Jinny <ggab.challenge@gmail.com>
2024-11-22 16:12:25 +0900

    refactor: for in -> forEach로 변경

--- src/View/outputView.js
+++ src/View/outputView.js
@@ -4,9 +4,11 @@ import { MESSAGE } from '../Constants/messages.js';
 export class Output {
   printRaceStatus(carNames, racingProgression) {
     Console.print(MESSAGE.EXECUTION_RESULT);
-    for (const i in carNames) {
-      Console.print(`${carNames[i]} : ${'-'.repeat(racingProgression[i])}`);
-    }
+
+    // TODO: for in 을 사용하면 상속된 속성도 돌 수 있어서 위험하다. for in -> forEach로 변경 
+    carNames.forEach((carName, index) => {
+      Console.print(`${carName} : ${'-'.repeat(racingProgression[index])}`);
+    });
     Console.print('');
   }
 
