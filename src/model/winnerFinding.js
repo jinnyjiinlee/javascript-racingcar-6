8a2cc9a5ddb24f154a31f22a162f024b17ee3543
Jinny <ggab.challenge@gmail.com>
2024-11-22 16:12:48 +0900

    refactor: 함수 분리

--- src/model/winnerFinding.js
+++ src/model/winnerFinding.js
@@ -1,13 +1,14 @@
-export const findWinners = (racingProgression) => {
-  // console.log('racingProgression: ', racingProgression)
-  const MaxNumber = Math.max(...racingProgression);
-
-  const findIndexArray = racingProgression
+const findIndexArray = (MaxNumber, racingProgression) =>
+  racingProgression
     .map((item, index) => {
       if (item === MaxNumber) return index;
-      else return -1;
+      return -1;
     })
     .filter((item) => item !== -1);
 
-  return findIndexArray;
+export const findWinners = (racingProgression) => {
+  const MaxNumber = Math.max(...racingProgression);
+  const winnersIndex = findIndexArray(MaxNumber, racingProgression);
+
+  return winnersIndex;
 };
